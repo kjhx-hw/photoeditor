c1d78b5469b6fbf7f9ee26a114937f78e5e6d9db
Kyle Huggins <5430818+kjhx@users.noreply.github.com>
2019-09-12 20:43:49 -0500

    Initial import

---
+++ PhotoEditor/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoEditor {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e) {
+            AboutDialog d = new AboutDialog();
+            d.ShowDialog();
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+![image-header](header.png)
+
+
+## Information
+
+Built by [Kyle Huggins](https://github.com/kjhx/) and [Noah Furby](https://github.com/fairytamerryles/) for COMP 4450.
+
+[Repository link](https://github.com/kjhx/photoeditor).
