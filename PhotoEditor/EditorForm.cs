80f37df323608ebccb5508486d4f5409c4031fd4
Super5432 <49763886+Super5432@users.noreply.github.com>
2019-09-21 14:21:47 -0500

    Built the editor form UI

---
+++ PhotoEditor/EditorForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoEditor
+{
+    public partial class EditorForm : Form
+    {
+        public EditorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EditorForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
